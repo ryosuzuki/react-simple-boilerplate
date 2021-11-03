ec8cf2f96bfb0b763671485ee80134da5f1a83ef
Ryo Suzuki <ryosuzk@gmail.com>
2021-11-03 07:06:11 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return <h1> Hello world!! </h1>
+  }
+}
+
+export default App
